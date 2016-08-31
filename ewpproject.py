7b24babefb39fa4f5eb8947772ae34dac284fe54
cylon <cylon@openmailbox.org>
2016-08-31 20:10:55 +0200

    Parsing methods are rewritten for each project into class.

--- ewpproject.py
+++ ewpproject.py
@@ -1,7 +1,59 @@
 # -*- coding: utf-8 -*-
-
+import xmltree
+from lxml import objectify
+    
 class EWPProject (object):
     
-    def __init__(self):
+    def __init__(self, xmlFile):
+                
+        self.xmlFile = xmlFile
+        self.xmltree = objectify.parse(xmlFile)
+        self.root = xmltree.getroot()
+
+    def parseProject(self):
         
-        pass
+        self.projectName = self.root.configuration.name
+        
+        self.chip = ''
+    
+        #TODO: parse into tree structure    
+        self.searchGroups(self.root)
+        
+        self.defines = []
+        self.includes = []
+        
+        for element in self.root.configuration.getchildren():
+            
+            if element.tag == 'settings':
+                for e in element.data.getchildren():
+     
+                    if e.tag == 'option':                
+                        if e.name.text == 'OGChipSelectEditMenu':                                          
+                            self.chip = e.state 
+                        elif e.name.text == 'CCDefines':
+                            for d in e.getchildren():
+                                if d.tag == 'state':
+                                    self.defines.append(d.text)
+                        elif e.name.text == 'CCIncludePath2':
+                            for d in e.getchildren():
+                                if d.tag == 'state':
+                                    self.includes.append(d.text)
+          
+    def displaySummary(self):
+             
+             
+        print ('Project Name:' + self.projectName)
+        print ('Project chip:' + self.chip)
+        print ('Project includes: ' + self.includes)
+        print ('Project defines: ' + self.defines)
+        
+    def searchGroups(self, xml):
+    
+        for element in xml.getchildren():
+    
+            if element.tag == 'group':
+                self.searchGroups(element)
+                
+            elif element.tag == 'file':  
+                print (element.name)
+        
--- uvprojxproject.py
+++ uvprojxproject.py
@@ -1,7 +1,36 @@
 # -*- coding: utf-8 -*-
-
+import xmltree
+from lxml import objectify
+    
 class UVPROJXProject (object):
     
-    def __init__(self):
+    def __init__(self, xmlFile):
         
-        pass
+        self.xmlFile = xmlFile
+        self.xmltree = objectify.parse(xmlFile)
+        self.root = xmltree.getroot()
+    
+    def parseProject (self):
+        
+        self.projectName = self.root.Targets.Target.TargetName
+        self.chip = self.root.Targets.Target.TargetOption.TargetCommonOption.Device
+        self.svd = self.root.Targets.Target.TargetOption.TargetCommonOption.SFDFile
+        self.includes = self.root.Targets.Target.TargetOption.TargetArmAds.Cads.VariousControls.IncludePath.text
+        self.memories = self.root.Targets.Target.TargetOption.TargetCommonOption.Cpu
+        
+        
+        for element in self.root.Targets.Target.Groups.getchildren():
+            
+            print ('GroupName: ' + element.GroupName.text)
+            for file in element.Files.getchildren():
+                print ('FileName: ' + file.FileName.text)
+                print ('FilePath: ' + file.FilePath.text)
+
+    def displaySummary(self):
+        
+        print ('Project Name:' + self.projectName)
+        print ('Project chip:' + self.chip)
+        print ('Project svd:' + self.svd)
+        print ('Project includes: ' + self.includes)
+        print ('Project: ' + self.memories)
+    
